3d87a5c78e7a5b5381e4657a7138d56ce46ae14a
Angelo <devsangelo@gmail.com>
2021-07-13 13:50:56 +0800

    Initial Commit

---
+++ APIDataAlamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIDataAlamofire
+//
+//  Created by Ruthlyn Huet on 7/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
